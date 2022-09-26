255d0c46b9a733dafb811843c11906857a6065c6
Javi Aguilar <122741+itsjavi@users.noreply.github.com>
2022-09-26 11:04:35 +0200

    Create mdx-layout-loader.js

---
+++ mdx-layout-loader.js
@@ -0,0 +1,27 @@
+const matter = require("gray-matter")
+
+const mdxLayoutLoader = function (source) {
+  const options = this.getOptions()
+  const { data, content } = matter(source)
+
+  // Here we convert frontmatter YAML to exported JS object, 
+  // and the layout as the default export as NextJS expects.
+
+  const preparedSource = `
+
+import ${options.layout.component} from '${options.layout.module}'
+
+export const __frontMatter = JSON.parse('${JSON.stringify(data)}');
+
+${content}
+
+export default ({ children }) => (
+    <${options.layout.component} ${options.layout.frontMatterProp}={__frontMatter}>
+        {children}
+    </${options.layout.component}>
+)
+`
+  return preparedSource
+}
+
+module.exports = mdxLayoutLoader
